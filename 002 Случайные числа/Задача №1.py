859249224717c044ec4f54af07a4dc832e9ab6c7
grigorii_bakhov <bg_1988@mail.ru>
2023-12-26 12:13:27 +1200

    26.12

---
+++ 002 Случайные числа/Задача №1.py
@@ -0,0 +1,5 @@
+from random import *
+a =randint(1,6)
+b =randint(1,6)
+c =randint(1,6)
+print(f'Выпало очков:\n{a} {b} {c}\n({a}+{b}+{c})/3={(a+b+c)//3}')
---
+++ 002 Случайные числа/Задача №2.py
@@ -0,0 +1,5 @@
+from random import *
+a =randint(1,6)
+b =randint(1,6)
+c =randint(1,6)
+print(f'Выпало очков:\n{a} {b} {c}\nЧисло {100*a+10*b+c}')
