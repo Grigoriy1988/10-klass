4eb24caf19baffad9d2f3b68449abde7de852b4d
Grigoriy <bg_1988@mail.ru>
2024-01-11 23:42:17 +1200

    26.12

--- 002 Случайные числа/Задача №2.py
+++ 002 Случайные числа/Задача №2.py
@@ -2,4 +2,4 @@ from random import *
 a =randint(1,6)
 b =randint(1,6)
 c =randint(1,6)
-print(f'Выпало очков:\n{a} {b} {c}\nЧисло {100*a+10*b+c}')
+print(f'Выпало очков:\n{a} {b} {c}\nЧисло {100*a+10*b+c}\nЕго квадрат {(100*a+10*b+c)**2}')
