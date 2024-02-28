84f592623dd921f5175134da8f333931bbc81a43
Prepod-i2 <bg_1988@mail.ru>
2024-02-28 16:03:19 +1200

    1

---
+++ 010/08.py
@@ -0,0 +1,7 @@
+# https://neerc.ifmo.ru/wiki/index.php?title=%D0%9D%D0%B0%D1%85%D0%BE%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5_%D0%BA%D0%BE%D0%BB%D0%B8%D1%87%D0%B5%D1%81%D1%82%D0%B2%D0%B0_%D1%80%D0%B0%D0%B7%D0%B1%D0%B8%D0%B5%D0%BD%D0%B8%D0%B9_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%BD%D0%B0_%D1%81%D0%BB%D0%B0%D0%B3%D0%B0%D0%B5%D0%BC%D1%8B%D0%B5
+#https://www.youtube.com/watch?v=O3k9OrwIvbA
+def p(n):
+    if n == 0:
+        return 1
+    else:
+
